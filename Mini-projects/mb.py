c815eb4a4766f2392a8e045ddd1eaa31d925b28f
Danilo Martins <danimarts.dm@gmail.com>
2020-11-09 01:15:04 +0200

    Add blynk functionality
    - Rate change virtual button works
    - Extra button to stop execution
    - temperature reading works. Polls every second.
    - Terminal does not work, especially if the temperature reading must work
    
    It's really hard to find good examples in python. Many people seem to use this for Arduinos and ESPs, which use C/C++ as their main language.
    We could use WiringPi, which is in C, but I preferred to keep everything simple in one language.

--- Mini-projects/mb.py
+++ Mini-projects/mb.py
@@ -9,11 +9,14 @@ import RPi.GPIO as GPIO
 import ES2EEPROMUtils
 import os
 import time
+import blynklib
 
+
+BLYNK_AUTH = "dbRneQ7iQIH-TkBb6ZoM0qqrNkUcf6Ew"
+
+blynk = blynklib.Blynk(BLYNK_AUTH)
 
 buzzer_pin = 13  # the buzzer pin in BCM mode
-
-# buzzer = None  # this will represent the buzzer component
 
 sample_count = 0  # the number of samples up to now
 
@@ -27,6 +30,7 @@ program_is_running = True  # this variable will be toggled when the start-stop b
 # parameters of the temperature sensor (from datasheet)
 Tc = 10e-3 # temperature coefficient, in V/℃
 V0 = 500e-3 # output voltage at 0 ℃, in V
+T_ambient = 0
 
 # to calculate runtime
 start = datetime.datetime.now()
@@ -35,6 +39,7 @@ def setup():
     # the following variables must be global
     global spi, cs, mcp, chan, buzzer
 
+    #region Setup ADC
     # create the spi bus 
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -46,28 +51,40 @@ def setup():
 
     # create an analog input channel on pin 1 
     chan = AnalogIn(mcp, MCP.P1)
-
-
-    # the toggle rate button pin (in BCM mode)
-    rate_pin = 23  # this is pin 16 in BOARD MODE
+    #endregion Setup ADC
 
     # the pin to start/stop printing to the console
     start_stop_pin = 24  # this is pin 18 in BOARD MODE 
 
-    # setting GPIO 23 (BCM) and GPIO 24 (BCM) as an input
-    GPIO.setup((rate_pin, start_stop_pin), GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # setting GPIO 24 (BCM) as an input
+    GPIO.setup(start_stop_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     #region Setup buzzer
     GPIO.setup(buzzer_pin, GPIO.OUT, initial=GPIO.LOW)
 
-    # adding a callback for when the button is clicked
-    GPIO.add_event_detect(rate_pin, GPIO.FALLING, callback=toggle_rate, bouncetime=300)
+    # adding a callback for when the start/stop button is clicked
     GPIO.add_event_detect(start_stop_pin, GPIO.FALLING, callback=start_stop, bouncetime=300)
+
+    # handling the virtual button clicks
+    @blynk.handle_event('write V1')
+    def write_virtual_pin_handler(pin, value):
+        toggle_rate()
+
+    @blynk.handle_event('read V2')
+    def read_virtual_pin_handler(pin):
+        blynk.virtual_write(pin, round(T_ambient, 3))
+
+
+    # an extra button to quit
+    @blynk.handle_event('write V3')
+    def write_virtual_pin_handler(pin, value):
+        exit()
 
 
 def fetch_scores():
 	data = eeprom.read_block(eeprom_index, 4)
 	print(data)
+
 
 def save_scores(data):
 	global eeprom_index
@@ -87,7 +104,7 @@ def save_scores(data):
 		#fetch_scores()
 		eeprom_index += 1
 
-def toggle_rate(_):
+def toggle_rate():
     """
         This function toggles the sampling time
     """
@@ -100,9 +117,9 @@ def toggle_rate(_):
         time_interval = 5
 
     elif time_interval == 5:
-        time_interval = 1
+        time_interval = 2
         
-    elif time_interval == 1:
+    elif time_interval == 2:
         time_interval = 10
     
     thread = threading.Timer(time_interval, print_values)
@@ -149,13 +166,14 @@ def beep():
 
 def print_header():
     # don't worry about the last column; it will be removed.
-    print("{:8s}\t{:9s}\t{:4s}\t\t{:s}".format("Time", "Sys Timer", "Temp", "Buzzer"))
+    print("{:8s}\t{:9s}\t{:4s}".format("Time", "Sys Timer", "Temp"))
 
 
 def print_values():
     # using thread as a global variable
     global thread
     global sample_count
+    global T_ambient
 
     thread = threading.Timer(time_interval, print_values)
     thread.daemon = True
@@ -185,7 +203,19 @@ def print_values():
     save_scores(eeprom_data)
 
     sample_count += 1
-    print("{:8s}\t{:9s}\t{:.3f}  C\t {:d}".format(current_time, sys_time, T_ambient, sample_count))
+
+    times_and_temperature = "{:8s}\t{:9s}\t{:.3f}  C".format(current_time, sys_time, T_ambient)
+
+    # printing to the Pi's console
+    print(times_and_temperature)
+
+    # printing to the Blynk console whenever blynk requests the temperature. really bad solution
+    # @blynk.handle_event('read V2')
+    # def my_virtual_pin_handler(pin):
+    #     blynk.virtual_write(0, times_and_temperature+'\n')   
+
+
+    
 
     # beep in the first sample and every 5th sample
     if sample_count == 1 or sample_count % 5 == 0:
@@ -195,10 +225,13 @@ def print_values():
 if __name__ == "__main__":
     try:
         setup()
+        # clearing the blynk logo
+        os.system("clear")
         print_header()
         print_values()
 
         while True:
+            blynk.run()
             pass
     
     except Exception as e:
